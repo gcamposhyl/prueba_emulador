97695c5695a4ea15bc5b428c7e198447fbd94d8d
Gabriel Campos <gcampos@hyl.cl>
2023-07-27 14:32:29 -0400

    First commit feature_functions

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,46 @@
+// Dependencias de functions
 const functions = require("firebase-functions");
+const cors = require("cors")({ origin: true });
 
-// // Create and deploy your first functions
-// // https://firebase.google.com/docs/functions/get-started
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+//import { db, storage} from '../src/config/firebase'
+//import { ref, listAll, getDownloadURL  } from "firebase/storage";
+
+// Function de prueba
+exports.helloWorld = functions.https.onRequest((request, response) => {
+    cors(request, response, () => {
+      const name = request.query.name || 'Anonymous'; // Obtener el parámetro "name" de la consulta
+      const data = request.body; // Obtener el objeto enviado en el cuerpo de la solicitud
+           
+      functions.logger.info("Hello logs!", { structuredData: true });
+      response.send(`Hello, ${name}, from Firebase!, obj: ${JSON.stringify(data['storage'])}`);
+    });
+  });
+  
+
+
+// Functions storage
+
+// Funcion que consulta todos los elementos en el directorio de un bucket, retorna arreglo con objetos
+/*exports.getAllFiles = functions.https.onRequest((request, response) => {
+    cors(request, request, () => {
+        const storageRef = ref(storage);
+        const imagesRef = ref(storage, 'imagenes');
+    })
+})*/
+
+
+// Funcion para guardar imagen, retorna url de imagen guardada
+// Funcion que consulte documento espefico y retorne url 
+// Funcion que elimine todos los archivos de la carpeta
+// Fuuncion que elimine archivo por id
+
+// Functions fireStore
+
+// Crear objeto con nuevos datos, void
+// Actualizar parte de un objeto, retorna objeto actualizado
+// Eliminar Objeto completo, retorna objeto eliminado
+// Listar todos los objetos, retorna arreglo con objetos
+// Listar objeto particular segun id, retorna objeto
+// * Consultar objeto segun dos condiciones
+
+
--- src/config/firebase.js
+++ src/config/firebase.js
@@ -5,6 +5,11 @@ import { getRemoteConfig, getValue, fetchAndActivate, fetchConfig, activate   } 
 
 import { initializeAppCheck, ReCaptchaV3Provider } from "firebase/app-check";
 
+import { getFunctions, connectFunctionsEmulator } from "firebase/functions";
+
+import { getStorage, connectStorageEmulator, ref, listAll, getDownloadURL  } from "firebase/storage";
+
+
 let firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
   authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
@@ -19,43 +24,98 @@ const firebaseApp = initializeApp(firebaseConfig);
 //const db = getFirestore(firebaseApp);
 const db = getFirestore();
 const remoteConfig = getRemoteConfig();
+const functions = getFunctions();
+const storage = getStorage();
+
+// Con esto me conecto a emulador firestore
+if (location.hostname === 'localhost'){
+  // Emulador firestore
+  connectFirestoreEmulator(db, '127.0.0.1', 8080);
+  // Emulador functions
+  connectFunctionsEmulator(functions, "127.0.0.1", 5001);
+  // Emulador storage
+  connectStorageEmulator(storage, "127.0.0.1", 9199);
+}
 
 // Con este recupero parametros en nube
 remoteConfig.settings.minimumFetchIntervalMillis = 36000;
 
-
 // Con este obtengo parametros de config
 const isFetched = await fetchAndActivate(remoteConfig);
 if(isFetched){
-    console.log("entro if")
-    let test = getValue(remoteConfig, "abc")
-    console.log(test)
+  let test = getValue(remoteConfig, "abc")
 } else {
-    let test = getValue(remoteConfig, "abc")
-    console.log("entro else")
-    console.log(test)
+  let test = getValue(remoteConfig, "abc")
 }
 
-// 3D695F20-3BBF-4E08-99BD-31A08217B659
-// token_dep
+// Create a storage reference from our storage service
+const storageRef = ref(storage);
+const imagesRef = ref(storage, 'imagenes');
+console.log(storageRef)
+console.log(imagesRef)
 
-//const appCheck = await initializeAppCheck(firebaseApp, {
-  //  provider: new ReCaptchaV3Provider('contrasena'),
-  
-    // Optional argument. If true, the SDK automatically refreshes App Check
-    // tokens as needed.
-    //isTokenAutoRefreshEnabled: true
- //});
 
-// Con esto me conecto a emulador firestore
-if (location.hostname === 'localhost'){
-    console.log("aqui necesito detectar que quiero cambiar a emulador")
-    connectFirestoreEmulator(db, '127.0.0.1', 8080);
+//Ejemplo consumir function en emulador
+const name = "Gabrielca"; // Cambia el nombre aquí
+const url = `http://127.0.0.1:5001/gabriellab-876b3/us-central1/helloWorld?name=${encodeURIComponent(name)}`;
+const dataToSend = { key1: 'value1', key2: 'value2' }; // El objeto que deseas enviar
+
+const allObj = {
+  "storage": storage,
+  "firestore": db,
+  "dataToSend": dataToSend
 }
+
+await fetch(url, {
+  method: 'POST', // Especificamos que la solicitud será de tipo POST
+  headers: {
+    'Content-Type': 'application/json', // Indicamos que el cuerpo de la solicitud es JSON
+  },
+  body: JSON.stringify(allObj), // Convertimos el objeto a formato JSON para enviarlo en el cuerpo
+})
+  .then(response => {
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
+    return response.text();
+  })
+  .then(data => {
+    console.log(data); // Imprime "Hello, Gabriella, from Firebase!" en la consola
+  })
+  .catch(error => {
+    console.error('Error:', error);
+  });
+ 
+
+// Find all the prefixes and items.
+listAll(imagesRef)
+  .then((res) => {
+    res.prefixes.forEach((folderRef) => {
+      console.log('Carpeta:', folderRef.name);
+      // Puedes llamar listAll() recursivamente en folderRef para obtener archivos dentro de esta carpeta.
+    });
+    res.items.forEach((itemRef) => {
+      console.log('Nombre del archivo:', itemRef.name);
+      // Obtener la URL de descarga de la imagen
+      getDownloadURL(itemRef)
+        .then((url) => {
+          console.log('URL de la imagen:', url);
+          // Aquí puedes usar la URL para mostrar la imagen en una etiqueta <img> o para otras operaciones
+        })
+        .catch((error) => {
+          console.error('Error al obtener la URL de la imagen:', error);
+        });
+    });
+  })
+  .catch((error) => {
+    console.log('Error al listar archivos:', error);
+  });
+
 
 export {
-    firebaseApp,
-    db,
-
+  firebaseApp,
+  db,
+  functions,
+  storage
 }
 
